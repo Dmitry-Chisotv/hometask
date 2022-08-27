dbb92962771670532294ca0cb25d3cd7fb6d7db4
Dmitry-Chisotv <111231261+Dmitry-Chisotv@users.noreply.github.com>
2022-08-27 13:24:19 +0300

    Update ex2.cs
    change the solution according to advise of teacher

--- ex2.cs
+++ ex2.cs
@@ -6,18 +6,11 @@ Console.Write("Введите третье число: ");
 int num3 = Convert.ToInt32(Console.ReadLine());
 
 int max_num = num1;
-if (max_num > num2)
-{
-}
-else
+if (max_num < num2)
 {
     max_num = num2;
 }
-
-if (max_num > num3)
-{
-}
-else
+if (max_num < num3)
 {
     max_num = num3;
 }
