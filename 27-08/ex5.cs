fdce6915227db8232c9c0d002da555bb387c2986
Dmitry c <hard45@inbox.ru>
2022-08-27 12:59:54 +0300

    new hometask 27-08

---
+++ 27-08/ex5.cs
@@ -0,0 +1,12 @@
+﻿//Задача 1: Напишите программу, которая принимает на вход случайно сгенерированное трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+//456 -> 5
+//782 -> 8
+//918 -> 1
+
+
+int rand = new Random (). Next(100, 1000);
+Console.WriteLine (rand);
+int rand1 = rand / 10;
+int digit_second = rand1 % 10;
+Console.WriteLine (digit_second);
---
+++ 27-08/ex6.cs
@@ -0,0 +1,41 @@
+﻿//Задача 2: Напишите программу, которая выводит третью цифру случайно сгенерированного числа (от 1 до 65536) или сообщает, что третьей цифры нет.
+//645 -> 5
+//78 -> третьей цифры нет
+//32679 -> 6
+
+int rand = new Random (). Next(1, 65536);
+Console.WriteLine (rand);
+if (rand < 99)
+{
+    Console.WriteLine ("третьей цифры нет");
+}
+
+if (rand < 999)
+{
+    if (99 < rand)
+    {
+        int digit_last = rand % 10;
+    Console.WriteLine (digit_last);
+    }
+}
+
+if (rand < 9999)
+{
+    if (999 < rand)
+    {
+        int digit_first = rand / 100;
+        int digit_last = rand % 10;
+        int digit_second = rand - digit_first * 100 - digit_last;
+        Console.WriteLine (digit_second / 10);
+    }
+}
+
+if (rand < 65536)
+{
+    if (9999 < rand)
+    {
+        int digit_1 = rand / 1000;
+        int digit_2 = (rand - digit_1*1000) / 100;
+    Console.WriteLine (digit_2);
+    }
+}
