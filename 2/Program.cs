8fc58e99b16520a48452b953f46b9b6f51eddf2f
D <hard45@inbox.ru>
2022-09-03 20:00:40 +0300

    dz

---
+++ 2/Program.cs
@@ -0,0 +1,26 @@
+﻿//Задача 2
+//Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
+
+
+Console.Write("Введите значение на оси координат x для точки А: ");
+int xa = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите значение на оси координат y для точки А: ");
+int ya = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите значение на оси координат z для точки А: ");
+int za = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите значение на оси координат x для точки B: ");
+int xb = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите значение на оси координат y для точки B: ");
+int yb = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите значение на оси координат z для точки B: ");
+int zb = Convert.ToInt32(Console.ReadLine());
+
+int x = xb - xa;
+int y = yb - ya;
+int z = zb - za;
+int a = x * x + y * y + z * z;
+
+double length = Math. Sqrt( a );
+
+Console.WriteLine("Расстояние между точками А и В в 3D пространстве: " + length);
---
+++ 3/Program.cs
@@ -0,0 +1,19 @@
+﻿//Задача 3
+
+//Напишите программу, которая принимает на вход число (N) и помещает в массив таблицу кубов чисел от 1 до N. Полученный массив вывести на экран.
+
+
+Console.Write("Введите число: ");
+int N = Convert.ToInt32(Console.ReadLine());
+
+int [] array = new int [N];
+
+int index = 0;
+int num = 1;
+while (index < N)
+{
+    array [index] = num * num * num;
+    Console.Write(array [index] + " ");
+    num++;
+    index++;
+} 
