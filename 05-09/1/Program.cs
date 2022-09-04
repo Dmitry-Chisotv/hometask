42e148088da8d95c29ffad6a55481266b32cd526
D <hard56@inbox.ru>
2022-09-05 01:29:43 +0300

    dz 05-09

---
+++ 05-09/1/Program.cs
@@ -0,0 +1,19 @@
+﻿//Задача 1: 
+//Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B. Не использовать функцию Math.Pow()
+//3, 5 -> 243
+//2, 4 -> 16
+
+Console.Write("Введите число А: ");
+int digit_A = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число В, в степень которого необходимо ввести число А: ");
+int digit_B = Convert.ToInt32(Console.ReadLine());
+
+int index = 1;
+int digit_ab = 1;
+while (index <= digit_B)
+{
+    digit_ab = digit_ab * digit_A;
+    index++;
+} 
+
+Console.WriteLine("А в степени В = " + digit_ab);
---
+++ 05-09/3/Program.cs
@@ -0,0 +1,38 @@
+﻿//Задача 3:*(Дополнительная, необязательная задача): 
+//Назовём число «интересным» если  произведение его цифр делится на их сумму. Напишите программу, которая заполняет массив на N «интересных» случайных целых чисел. (Каждый эл-т массива должен быть сгенерирован случайно)
+
+
+Console.Write("Введите число N: ");
+int ar_n = Convert.ToInt32(Console.ReadLine());
+
+int i = 0;
+int count = 0;
+
+while (count < ar_n) 
+{
+ int rand = new Random().Next(1, 999);
+ int tmp = rand;
+ int sum = 0;
+ int product = 1;
+   while (tmp > 0)
+   {
+     int n = tmp % 10;
+     sum += n;
+     product = product * n;
+     tmp /= 10;
+   }
+ 
+ int [] interest_digits = new int [ar_n];
+ int j = 1; 
+
+ if (product % sum == 0)
+ {
+   if ( product != 0)
+   {
+    interest_digits [j] = rand;
+    Console.Write(interest_digits [j] + ", ");
+    count ++;
+   }
+ }
+ i++;
+}
