11db5e3292d7c3c7f388ad4882baaba336ffb054
D <hard56@inbox.ru>
2022-09-15 03:36:50 +0300

    dz unit 6

---
+++ Lesson6/1/Program.cs
@@ -0,0 +1,24 @@
+﻿//Задача 1: Программа запрашивает натуральное число M, пользователь вводит с клавиатуры M чисел. 
+//Посчитайте, сколько чисел больше 0 ввёл пользователь.
+//0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 3
+
+int MforConsumer = new Random().Next(1, 10);
+int countM = 0;
+int count = 0;
+//Console.WriteLine(MforConsumer);
+int tmp = MforConsumer;
+
+while (countM < MforConsumer)
+{
+    Console.WriteLine("Введите " + tmp + " чисел");
+    int n = Convert.ToInt32(Console.ReadLine());  
+    if (n > 0)
+    {
+        count +=1;
+    }
+countM +=1;
+tmp -= 1;
+}
+
+Console.WriteLine("Пользователем введено " + count + " положительных чисел");
---
+++ Lesson6/2/Program.cs
@@ -0,0 +1,32 @@
+﻿//Задача 2: Напишите программу, которая найдёт точку пересечения двух прямых, 
+//заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; 
+//значения b1, k1, b2 и k2 задаются пользователем.
+//Рекомендую учесть три возможных случая расположения двух прямых.
+//b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+
+
+Console.WriteLine("Для поиска точки пересечения двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2" );
+Console.WriteLine("Введите значение b1 = " );
+double b1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите значение k1 = " );
+double k1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите значение b2 = " );
+double b2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите значение k2 = " );
+double k2 = Convert.ToInt32(Console.ReadLine());
+
+if (k1 == k2)
+{
+    Console.WriteLine("Данные прямые - параллельны" );
+}
+
+if (k1 != k2)
+{  
+    if (k2 == -1/k1)
+    {
+     Console.WriteLine("Данные прямые - перпендикулярны" );   
+    }
+    double x = (b2-b1)/(k1-k2);
+    double y = k1 * x + b1;
+    Console.WriteLine("Точка пересечения двух прямых: " + x + "; " + y);
+}
